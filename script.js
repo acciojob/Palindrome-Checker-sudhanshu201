bac2a448944a9787958864aa180297cb97222594
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-01-22 20:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 
 function palindrome(str){
 
+    // convert the string to lowercase
+    str = str.toLowerCase();
+    // remove non-alphanumeric characters
+    str = str.replace(/[^a-z0-9]/gi, "");
+    // reverse the string
+    let reversedStr = str.split("").reverse().join("");
+    // check if the reversed string is equal to the original string
+    return str === reversedStr;
 }
+
 module.exports = palindrome
